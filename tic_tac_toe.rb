eea7017ed0a334109660b65d47fdd96718c634a7
icarus2176 <ashe.kemuri@gmail.com>
2023-03-09 15:36:49 -0600

    create new_game, turn, and play_game methods for Game class. and fix attr_accessors

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,8 +1,8 @@
 class Player
-  attr_accessor @name, @symbol
+  attr_accessor @symbol
 
-  def initialize(name, symbol)
-    @name, @symbol = name, symbol
+  def initialize(symbol)
+    @symbol = symbol
   end
 end
 
@@ -11,11 +11,11 @@ class Board
   @spaces = Array.new(9)
 
   def display
-    puts " " + @spaces[0] + " | " + @spaces[1] + " | " + @spaces[2] \
-         "___ ___ ___" \
-         " " + @spaces[3] + " | " + @spaces[4] + " | " + @spaces[5] \
-         "___ ___ ___" \
-         " " + @spaces[6] + " | " + @spaces[7] + " | " + @spaces[8]
+    puts "1) " + @spaces[0] + " |2) " + @spaces[1] + " |3) " + @spaces[2] \
+         "_____ _____ _____" \
+         "4) " + @spaces[3] + " |5) " + @spaces[4] + " |6) " + @spaces[5] \
+         "_____ _____ _____" \
+         "7) " + @spaces[6] + " |8) " + @spaces[7] + " |9) " + @spaces[8]
   end
 
   def change_space(symbol, position)
@@ -26,6 +26,8 @@ end
 class Game
   def new_game
     board = Board.new
+    player1 = Player.new("X")
+    player2 = Player.new("O")
   end
 
   def play_game
@@ -42,7 +44,13 @@ class Game
   end
   
   def turn(player)
-
+    space
+    while board.@spaces[space]
+      space = gets "Choose an empty space."
+      space = space.chomp.to_i - 1
+    end
+    
+    board.@spaces[space] = player.symbol
   end
 
   def win?(player)
